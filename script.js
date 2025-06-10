49b54c8c4d396b8cf324fa7d0ee70ae4db51267b
Ziyodabonu <159676375+ziyoda-art@users.noreply.github.com>
2025-06-10 21:17:09 +0300

    .

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-const buttons = document.querySelectorAll('.btn');
-const calDisplay = document.querySelector('.calDisplay');
-buttons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        console.log(btn.textContent);
-        if (btn.textContent === 'ac') {
+let buttons = document.querySelectorAll('.btn');
+// buttons ni document dan .btn classi orqali chaqirib olamiz
+
+let calDisplay = document.querySelector('.calDisplay');
+// calculator yozuvini chiqaradigan p ni chaqiryapmiz 
+
+buttons.forEach((btn)=> {
+    // buttons ni forEach orqali har bir elementni btn nomi ga beramiz
+
+    btn.addEventListener('click', ()=> {
+    // btn ga click event qo'shamiz bu yerda click bo'lganda function ishga tushadi
+
+        console.log(btn.textContent)
+    // btn larni  ni console ga chiqaramiz
+
+
+        if ( btn.textContent === "ac"){
+            // agar ac tugmasi bosilsa
+
             calDisplay.textContent = '0';
+            // p ni 0 ga o'zgartiramiz
+
             return;
         }
 
-        if (calDisplay.textContent === '0') {
-            calDisplay.textContent = btn.textContent;
-            return;
-        } else {
+
+        if(calDisplay.textContent === '0'){
+            // agar p 0 bo'lsa
+            calDisplay.textContent  = btn.textContent;
+            // o'sha p ni btn.textContent ga o'zgartiramiz
+        }else{ //yoki
             calDisplay.textContent = calDisplay.textContent + btn.textContent;
+            // p qiymati = p qiymati + btn.textContent ga o'zgartiramiz
         }
-        // calDisplay.textContent = btn.textContent
+
+        if (calDisplay.textContent.length > 10) {
+            // agar p uzunligi 12 dan katta bo‘lsa
+            calDisplay.textContent = ' undefined';
+        } else if (calDisplay.textContent.length >= 9) {
+            // agar p uzunligi 9 ga teng yoki 9 dan katta bo‘lsa
+            calDisplay.style.fontSize = '3rem';
+        } else {
+            // qolgan oddiy hollarda
+            calDisplay.style.fontSize = '4rem';
+        }
     })
 })
